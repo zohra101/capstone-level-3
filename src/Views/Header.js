90c8b552bf080f8ff2730c22c3c190daab64addf
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2025-02-12 19:34:24 -0600

    Changed images to React imports

--- src/Views/Header.js
+++ src/Views/Header.js
@@ -1,4 +1,7 @@
 import React from "react";
+import siteHeader from "../assets/images/siteHeader_4kp_rev2.png";
+import linkedInIcon from "../assets/icons/icon_linkedin.png";
+import emailIcon from "../assets/icons/icon_email_coral.png";
 
 export function Header() {
 	return (
@@ -6,26 +9,29 @@ export function Header() {
 			<header>
 				<div>
 					<img
+						id="siteHeader"
 						alt="siteHeader"
 						width="100%"
-						src="../src/assets/images/siteHeader_4kp_rev2.png"
+						src={siteHeader}
 					/>
 				</div>
 				<div>
 					<a
-						class="ms-2"
+						className="ms-2"
 						href="https://www.linkedin.com/in/alexmarjanovic">
 						<img
+							id="linkedInIcon"
 							alt="LinkedIn"
 							style={{height: "40px", width: "40px"}}
-							src="../src/assets/icons/icon_linkedin.png"
+							src={linkedInIcon}
 						/>
 					</a>
 					<a href="contact.html#sendMessage">
 						<img
+							id="emailIcon"
 							alt="email"
 							style={{height: "30px", width: "30px"}}
-							src="../src/assets/images/email_coral.png"
+							src={emailIcon}
 						/>
 					</a>
 				</div>
